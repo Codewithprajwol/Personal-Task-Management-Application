9aba16a51013eacfb1f45e6e7b784b37d0d58030
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-27 16:30:03 +0545

    wip: add interface to footer task component

--- Frontend/src/components/task/_taskFooter.tsx
+++ Frontend/src/components/task/_taskFooter.tsx
@@ -1,12 +1,19 @@
 import { Box, Button, FormControlLabel, Switch } from '@mui/material'
 import {ReactElement,FC} from 'react'
-
-export const TaskFooter:FC=():ReactElement=>{
+import { ITaskFooter } from './interfaces/ITaskFooter'
+import PropTypes from 'prop-types'
+export const TaskFooter:FC<ITaskFooter>=(props):ReactElement=>{
+        const {onStatusChange=(e)=>console.log(e),onClick=(e)=>console.log(e)}=props
         return (
-         <Box display={"flex"} alignItems={"center"} justifyContent={"space-between"} mb={3}>
-                <FormControlLabel label="In Progress" control={<Switch color='warning' />} />
-                <Button color='success' variant="contained" size="small">Mark Complete</Button>
+         <Box display={"flex"} alignItems={"center"} justifyContent={"space-between"} >
+                <FormControlLabel label="In Progress" control={<Switch onChange={(e)=>onStatusChange(e)} color='warning' />} />
+                <Button color='success' variant="contained" size="small" sx={{color:"#ffffff"}} onClick={(e)=>onClick(e)}>Mark Complete</Button>
          </Box>
         )   
 
+}
+
+TaskFooter.propTypes={
+        onStatusChange:PropTypes.func,
+        onClick:PropTypes.func,
 }
