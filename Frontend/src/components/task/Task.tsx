b0e08908e507335d6bd10acb04df2d44cd523b6d
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-27 16:50:31 +0545

    wip: add interfaces and propTypes in Task

--- Frontend/src/components/task/Task.tsx
+++ Frontend/src/components/task/Task.tsx
@@ -3,10 +3,23 @@ import { ReactElement, FC } from "react";
 import { TaskHeader } from "./_taskHeader";
 import { TaskDescription } from "./_taskDescription";
 import { TaskFooter } from "./_taskFooter";
+import { ITask } from "./interfaces/ITask";
+import PropTypes from "prop-types";
+import { Status } from "../createTaskForm/enums/Status";
+import { Priority } from "../createTaskForm/enums/Priority";
 
-const Task: FC = (): ReactElement => {
+const Task: FC<ITask> = (props): ReactElement => {
+  const {
+    title = "hello",
+    date = new Date(),
+    status=Status.completed,
+    priority=Priority.normal,
+    description = "do your work as soon as possible",
+    onStatusChange=(e)=>console.log(e),
+    onClick=(e)=>console.log(e),
+  } = props;
   return (
-    <Box
+    <Box 
       display="flex"
       width="100%"
       justifyContent={"flex-start"}
@@ -14,18 +27,28 @@ const Task: FC = (): ReactElement => {
       mb={4}
       p={2}
       sx={{
-        width:'100%',
-        backgroundColor:'background.paper',
-        borderRadius:'8px',
-        border:'1px solid',
-        borderColor:'error.light'
+        width: "100%",
+        backgroundColor: "background.paper",
+        borderRadius: "8px",
+        border: "1px solid",
+        borderColor: "error.light",
       }}
     >
-      <TaskHeader/>
-      <TaskDescription/>
-      <TaskFooter />
+      <TaskHeader title={title} date={date} />
+      <TaskDescription description={description} />
+      <TaskFooter onStatusChange={onStatusChange} onClick={onClick} />
     </Box>
   );
 };
+
+Task.propTypes={
+    title:PropTypes.string,
+    date:PropTypes.instanceOf(Date),
+    description:PropTypes.string,
+    onStatusChange:PropTypes.func,
+    onClick:PropTypes.func,
+    priority:PropTypes.string,
+    status:PropTypes.string
+}
 
 export default Task;
