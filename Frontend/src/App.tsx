00f7044cad0199cd679ee2d4029d6be21ffc49ab
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-01-30 10:51:58 +0545

    wip:-create function to fetch API request

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,21 @@
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import {FC,ReactElement}from 'react'
+import  {FC,ReactElement}from 'react'
 import { customTheme } from './themes/CusotmTheme'
 import Dashboard from './pages/dashboard/Dashboard'
+import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient=new QueryClient()
 const App:FC= ():ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
+    <QueryClientProvider client={queryClient}>
+       <ThemeProvider theme={customTheme}>
       <CssBaseline/>
     <Dashboard/>
     </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={false}/>
+  </QueryClientProvider>
+   
   )
 }
 
